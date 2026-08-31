59d102dd474cc3ed543641f4c6716d2500299c65
nkoji21 <133028205+nkoji21@users.noreply.github.com>
2026-09-01 01:26:36 +0900

    fix(blog): ツイートの埋め込みを中央に置く
    X の埋め込みは iframe に margin が直接入るため、
    包む要素の flex だけでなく CSS でも上書きしている。

--- src/components/blog/tweet-embed.tsx
+++ src/components/blog/tweet-embed.tsx
@@ -21,7 +21,7 @@ export function TweetEmbed({ url }: { url: string }) {
   }, []);
 
   return (
-    <>
+    <div className="flex justify-center">
       <blockquote className="twitter-tweet" data-lang="ja" data-dnt="true">
         <a href={url}>{url}</a>
       </blockquote>
@@ -29,6 +29,6 @@ export function TweetEmbed({ url }: { url: string }) {
         src="https://platform.twitter.com/widgets.js"
         strategy="lazyOnload"
       />
-    </>
+    </div>
   );
 }
